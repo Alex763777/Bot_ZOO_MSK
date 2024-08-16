918d66812ba603b37fafa249d184ca5b507ef23c
Alex763777 <alex1977.aka@gmail.com>
2024-08-16 23:41:00 +0400

    1запуск

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandle
 from config import TELEGRAM_TOKEN
 from handlers import start, quiz, button, help_command
 
-if __name__ == '__main__':  # Главная функция, где происходит настройка и запуск бота
-    # Создаю приложение и передаю ему токен ZOO-бота
+# Главная функция, где происходит настройка и запуск бота
+if __name__ == '__main__':
+    # Создаем приложение и передаем ему токен нашего бота
     app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 
-    # Добавляю обработчики команд и кнопок
+    # Добавляем обработчики команд и кнопок
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("quiz", quiz))
     app.add_handler(CallbackQueryHandler(button))
     app.add_handler(CommandHandler("help", help_command))
 
-    app.run_polling()  # Запуск бота в режиме polling
+    # Запуск бота в режиме polling
+    app.run_polling()
